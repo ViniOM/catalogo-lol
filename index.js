f4fe6d73ce8410798bc7438b50e2ae0be37b7024
Vinicios <vini.webdeveloper@gmail.com>
2022-03-19 19:46:59 -0300

    confirm

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); 
 app.use(express.urlencoded());
