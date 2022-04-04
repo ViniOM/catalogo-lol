da09946390d941b94f95c877487be4ed3c58ebd2
Vinicios <vini.webdeveloper@gmail.com>
2022-04-04 16:16:38 -0300

    back

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,17 @@
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize")
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connect = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-});
+const connect = new Sequelize(
+  process.env.DB_BASE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres"
+  }
+);
 
-module.exports = connect;
+
+module.exports = connect; 
