b9131ca4a7a83267b3d6b66c00dfa07c3e01e1e7
Vinicios <vini.webdeveloper@gmail.com>
2022-04-04 15:53:44 -0300

    att connect postgress

--- src/database/db.js
+++ src/database/db.js
@@ -1,17 +1,9 @@
-const Sequelize = require("sequelize")
+const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connect = new Sequelize(
-  process.env.DB_BASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: 5432,
-    dialect: "postgres"
-  }
-);
+const connect = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+});
 
-
-module.exports = connect; 
+module.exports = connect;
